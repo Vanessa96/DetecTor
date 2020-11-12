668ee9683e65bcb085d7b0ee7e7ed8c64b6b0be6
Qingqing Cao <qqcao.cs@gmail.com>
2020-11-12 15:26:20 -0500

    up

--- cg/node.py
+++ cg/node.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
+
+from collections import defaultdict
 from dataclasses import dataclass
 from dataclasses import field
 from typing import Any
+
+from transformers import AutoConfig
 
 
 @dataclass
@@ -70,21 +74,34 @@ class LinearModel(nn.Module):
 #         hidden_states = self.dense(hidden_states)
 #         hidden_states = self.intermediate_act_fn(hidden_states)
 #         return hidden_states
+def resize_graph(dot, size_per_element=0.15, min_size=12):
+    """Resize the graph according to how much content it contains.
+    Modify the graph in place.
+    """
+    # Get the approximate number of nodes and edges
+    num_rows = len(dot.body)
+    content_size = num_rows * size_per_element
+    size = max(min_size, content_size)
+    size_str = str(size) + "," + str(size)
+    dot.graph_attr.update(size=size_str)
 
 
 if __name__ == '__main__':
     import torch
     from transformers import BertModel
+    from graphviz import Digraph
 
     # from transformers.modeling_bert import BertIntermediate
 
-    inputs = torch.randint(1000, size=(1, 100)).long()
-    model = BertModel.from_pretrained("prajjwal1/bert-tiny", torchscript=True)
-
-    trace = torch.jit.trace(model, inputs)
-    fc_graph = trace.inlined_graph
-    config = model.config
+    config = AutoConfig.from_pretrained("prajjwal1/bert-tiny")
     config.hidden_act = 'gelu_fast'
+    config.torchscript = True
+    model = BertModel(config)
+    inputs = torch.randint(1000, size=(1, 100)).long()
+    trace = torch.jit.trace(model, inputs)
+    # torch.jit.save(trace, "traced_bert-tiny.pt")
+
+    fc_graph = trace.inlined_graph
     # fc_model = BertIntermediate(config)
     # fc_model = LinearModel(config)
     # input_len = 32
@@ -93,49 +110,173 @@ if __name__ == '__main__':
     # fc_graph = fc_trace.inlined_graph
     fc_input_nodes = {i.debugName(): i for i in fc_graph.inputs()}
     fc_output_nodes = {i.debugName(): i for i in fc_graph.outputs()}
-    data_nodes = {}
+    data_nodes = dict()
     nodes = []
     gi_nodes = []
     go_nodes = []
     ops = set()
+    for i, io_node in enumerate(fc_graph.inputs()):
+        name = io_node.debugName()
+        node_type = io_node.type()
+        if isinstance(io_node.type(), torch.TensorType):
+            dtype = io_node.type().scalarType()  # Float
+            shape = io_node.type().sizes()  # list of int
+            ni_node = DataNode(name, dtype, shape)
+        else:
+            ni_node = DataNode(name, node_type, [])
+        gi_nodes.append(ni_node)
+        data_nodes[name] = ni_node
+
     for n in fc_graph.nodes():
         node_inputs = list(n.inputs())
         node_outputs = list(n.outputs())
         node_id = node_outputs[0].debugName()
-        node_scope = n.scopeName().replace('__module.', '')
+        node_scope = n.scopeName().replace('__module.', '').split('/')[-1]
         node_op = n.kind()
         ops.add(node_op)
         in_nodes = []
         for ni in node_inputs:
-            if isinstance(ni.type(), torch.TensorType):
+            name = ni.debugName()
+            node_type = ni.type()
+            if isinstance(node_type, torch.TensorType):
                 dtype = ni.type().scalarType()  # Float
                 shape = ni.type().sizes()  # list of int
                 # TODO: check how to detect if this is a parameter data node
-                ni_node = DataNode(ni.debugName(), dtype, shape)
-                in_nodes.append(ni_node)
-                if ni.debugName() in fc_input_nodes:
-                    gi_nodes.append(ni_node)
             else:
                 # need to handle int, int[], Long(), Device, bool, None
-                pass
-        no_nodes = []
-        for no in node_outputs:
-            if isinstance(no.type(), torch.TensorType):
-                dtype = no.type().scalarType()  # Float
-                shape = no.type().sizes()  # list of int
-                # TODO: check how to detect if this is a parameter data node
-                no_node = DataNode(no.debugName(), dtype, shape)
-                no_nodes.append(no_node)
-                if no.debugName() in fc_output_nodes:
-                    go_nodes.append(no_node)
+                dtype = node_type
+                shape = []
+            ni_node = data_nodes.get(name, DataNode(name, dtype, shape))
+            in_nodes.append(ni_node)
+            data_nodes[name] = ni_node
+        out_nodes = []
+        for node_out in node_outputs:
+            name = node_out.debugName()
+            node_type = node_out.type()
+            if isinstance(node_type, torch.TensorType):
+                dtype = node_out.type().scalarType()  # Float
+                shape = node_out.type().sizes()  # list of int
             else:
                 # need to handle int, int[], Long(), Device, bool, None
-                pass
-        op_node = OpNode(node_id, node_scope, node_op, in_nodes, no_nodes)
+                dtype = node_type
+                shape = []
+            out_node = data_nodes.get(name, DataNode(name, dtype, shape))
+            out_nodes.append(out_node)
+            data_nodes[name] = out_node
+        op_node = OpNode(node_id, node_scope, node_op, in_nodes, out_nodes)
         nodes.append(op_node)
     graph = Graph('bert-tiny', nodes, gi_nodes, go_nodes)
     # print(graph)
+    max_o = 0
+    sn = 0
+    tn = 0
     for i, n in enumerate(graph.nodes, 1):
-        print(i, n)
-    print(ops)
+        max_o = max(max_o, len(n.outputs))
+        tn += 1
+        if n.scope:
+            sn += 1
+            print(i, n.scope)
+    print(ops, max_o, sn, tn)
 
+    node_attr = dict(style='filled',
+                     shape='box',
+                     align='left',
+                     fontsize='12',
+                     ranksep='0.1',
+                     height='0.2')
+
+    dot = Digraph(node_attr=node_attr, graph_attr=dict(size="12,12"))
+    scope_nodes = defaultdict(list)  # scope to nodes map
+    import pygtrie
+    scope_trie = pygtrie.StringTrie(separator='.')
+    for node in graph.nodes:
+        op = node.op
+        scope = node.scope
+        if not scope:
+            # no scope nodes belong to the root graph
+            dot.node(node.id, label=op)
+            for inp in node.inputs:
+                dot.edge(inp.id, node.id)
+        else:
+            # if scope_trie.has_key(scope):
+            #     scope_nodes = scope_trie[scope]
+            #     scope_nodes.append(node)
+            #     scope_trie[scope] = scope_nodes
+            # else:
+            #     scope_trie[scope] = [node]
+            # scope_trie[scope]
+            scope_nodes[scope].append(node)
+
+    dot_graphs = dict()  # scope to subgraph map
+    added = set()
+
+    # order matters, use scope_nodes to traverse scope
+    # longest scope,
+    for node in graph.nodes:
+        scope = node.scope
+        if not scope:
+            continue
+        sub_scopes = scope.split('.')
+        num_scopes = len(sub_scopes)
+        # create subgraph based on scope depth
+        if num_scopes == 1:
+            # add last
+            if scope not in dot_graphs:
+                sg = Digraph('cluster_' + scope)
+                print('build nodes and edges', scope)
+                for n in scope_nodes[scope]:
+                    sg.node(n.id, label=scope + '-' + n.op)
+                    for inp in n.inputs:
+                        sg.edge(inp.id, n.id)
+                sg.body.append(f'label={scope}')
+                dot_graphs[scope] = sg
+                dot.subgraph(sg)
+            else:
+                print('skip', scope)
+            # sg.body.append('color=lightblue')
+        else:  # > 1
+            assert num_scopes > 1
+            print(sub_scopes)
+            for si in range(num_scopes, 0, -1):
+                p_scope = '.'.join(sub_scopes[0:si - 1])
+                if p_scope not in dot_graphs:
+                    p_sg = Digraph('cluster_' + p_scope)
+                    print('[parent] build nodes and edges', p_scope)
+                    for n in scope_nodes[p_scope]:
+                        p_sg.node(n.id, label=p_scope + '-' + n.op)
+                        for inp in n.inputs:
+                            p_sg.edge(inp.id, n.id)
+                    p_sg.body.append(f'label={p_scope}')
+                    dot_graphs[p_scope] = p_sg
+                else:
+                    print('skip [parent]', p_scope)
+                    p_sg = dot_graphs[p_scope]
+                child_scope = '.'.join(sub_scopes[0:si])
+                if child_scope not in dot_graphs:
+                    c_sg = Digraph('cluster_' + child_scope)
+                    print('[child] build nodes and edges', child_scope)
+                    for n in scope_nodes[child_scope]:
+                        c_sg.node(n.id, label=child_scope + '-' + n.op)
+                        for inp in n.inputs:
+                            c_sg.edge(inp.id, n.id)
+                    c_sg.body.append(f'label={child_scope}')
+                    dot_graphs[child_scope] = c_sg
+                else:
+                    print('skip [child]', child_scope)
+                    c_sg = dot_graphs[child_scope]
+                add_id = child_scope + '->' + p_scope
+                if add_id not in added:
+                    added.add(add_id)
+                    p_sg.subgraph(c_sg)
+                if si == 2:  # must happen last, create inner subgraph first
+                    add_id = p_scope + '->' + 'dot'
+                    if add_id not in added:
+                        added.add(add_id)
+                        dot.subgraph(p_sg)
+                        print('add [dot]', add_id)
+                    else:
+                        print('skip [dot]', add_id)
+                # dot_graphs[p_scope].subgraph(dot_graphs[child_scope])
+
+    resize_graph(dot)
+    dot.render('bert-tiny.gv', view=True)
