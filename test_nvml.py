4a5905e560c51be0886d4e6ded5c02f8bb089bc3
Qingqing Cao <qqcao.cs@gmail.com>
2020-11-15 23:49:12 -0500

    up test

--- test_nvml.py
+++ test_nvml.py
@@ -1,6 +1,7 @@
 import datetime
 from time import sleep
 from py3nvml.py3nvml import *
+
 nvmlInit()
 print("Driver Version: {}".format(nvmlSystemGetDriverVersion()))
 # e.g. will print:
@@ -34,39 +35,48 @@ To represent processor clock samples.
 NVML_MEMORY_CLK_SAMPLES = 6
 To represent memory clock samples.
 """
-sample_types = [0, 1, 2, 5, 6]
+sample_types = [0, 1, 2]
 powers = [[] for _ in range(len(handles))]
 now = datetime.datetime.now()
-last_timestamp = int(now.timestamp()*1e6)
+last_timestamp = int(now.timestamp() * 1e6)
 sleep(1)
 while n < 10:
-  now = datetime.datetime.now()
-  for i, h in enumerate(handles):
-    p1 = nvmlDeviceGetPowerUsage(h)
-    # pids = nvmlDeviceGetComputeRunningProcesses(h) #nvmlDeviceGetAccountingPids(h)
-    # print(';'.join([f'{p.pid}={p.usedGpuMemory}' for p in pids]))
-    st = nvmlDeviceGetUtilizationRates(h)
-    # st = [nvmlDeviceGetAccountingStats(h, p.pid) for p in pids]
-    # print(i, )
-    samples = [(nvmlSamplingType_t(sample_type).name, [(datetime.datetime.fromtimestamp(s.timeStamp/1e6).isoformat(), s.sampleValue.uiVal) for s in nvmlDeviceGetSamples(h, sample_type, last_timestamp)[1]]) for sample_type in sample_types]
-    # _, samples = nvmlDeviceGetSamples(h, 3, 0)
-    # samples = [f'{datetime.datetime.fromtimestamp(s.timeStamp/1e6).isoformat()}={s.sampleValue.uiVal}' for s in samples]
-    p = (now.isoformat(), p1, str(st), len(samples), samples)
-    powers[i].append(p)
-    print(i, p[0], p[1], p[2], p[3])
-    for (name, pt) in p[4]:
-      print('\t', name, len(pt))
-      for ts, tv in pt:
-        print('\t\t', ts, tv)
-  last_timestamp = int(now.timestamp()*1e6)
-  sleep(1)
-  n+=1
+    now = datetime.datetime.now()
+    for i, h in enumerate(handles):
+        p1 = nvmlDeviceGetPowerUsage(h)
+        # pids = nvmlDeviceGetComputeRunningProcesses(h) #nvmlDeviceGetAccountingPids(h)
+        # print(';'.join([f'{p.pid}={p.usedGpuMemory}' for p in pids]))
+        st = nvmlDeviceGetUtilizationRates(h)
+        # st = [nvmlDeviceGetAccountingStats(h, p.pid) for p in pids]
+        # print(i, )
+        samples = [(nvmlSamplingType_t(sample_type).name, [(
+                                                           datetime.datetime.fromtimestamp(
+                                                               s.timeStamp / 1e6).isoformat(),
+                                                           s.sampleValue.uiVal)
+                                                           for s in
+                                                           nvmlDeviceGetSamples(
+                                                               h, sample_type,
+                                                               last_timestamp)[
+                                                               1]]) for
+                   sample_type in sample_types]
+        # _, samples = nvmlDeviceGetSamples(h, 3, 0)
+        # samples = [f'{datetime.datetime.fromtimestamp(s.timeStamp/1e6).isoformat()}={s.sampleValue.uiVal}' for s in samples]
+        p = (now.isoformat(), p1, str(st), len(samples), samples)
+        powers[i].append(p)
+        print(i, p[0], p[1], p[2], p[3])
+        for (name, pt) in p[4]:
+            print('\t', name, len(pt))
+            for ts, tv in pt:
+                print('\t\t', ts, tv)
+    last_timestamp = int(now.timestamp() * 1e6)
+    sleep(1)
+    n += 1
 
 print("======all samples======")
 for i, power in enumerate(powers):
-  for p in power:
-    print(i, p[0], p[1], p[2], p[3])
-    for (n, pt) in p[4]:
-      print('\t', n, len(pt))
-      for ts, tv in pt:
-        print('\t\t', ts, tv)
+    for p in power:
+        print(i, p[0], p[1], p[2], p[3])
+        for (n, pt) in p[4]:
+            print('\t', n, len(pt))
+            for ts, tv in pt:
+                print('\t\t', ts, tv)
