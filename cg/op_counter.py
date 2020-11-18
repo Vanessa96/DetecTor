eee8b1c28fd19a313c737b3669169bad0ac2c6a8
Qingqing Cao <qqcao.cs@gmail.com>
2020-11-18 10:16:36 -0500

    up op impl note

--- cg/op_counter.py
+++ cg/op_counter.py
@@ -17,75 +17,78 @@ https://github.com/facebookresearch/fvcore/blob/master/fvcore/nn/jit_handles.py
 
 import math
 
+
+# ops marked with # p1 are implemented
+# ops marked with # only are planned with low priority
 op_counters = {
     'aten::Int': 'aten_Int',
     'aten::ScalarImplicit': 'aten_ScalarImplicit',
-    'aten::abs': 'aten_abs',
-    'aten::add': 'aten_add',
-    'aten::add_': 'aten_add_',
-    'aten::addmm': 'aten_addmm',
-    'aten::arange': 'aten_arange',
-    'aten::bmm': 'aten_bmm',
-    'aten::cat': 'aten_cat',
-    'aten::clone': 'aten_clone',
-    'aten::constant_pad_nd': 'aten_constant_pad_nd',
+    'aten::abs': 'aten_abs',  #
+    'aten::add': 'aten_add',  # p1
+    'aten::add_': 'aten_add_',  # p1
+    'aten::addmm': 'aten_addmm',  # p1
+    'aten::arange': 'aten_arange',  #
+    'aten::bmm': 'aten_bmm',  # p1
+    'aten::cat': 'aten_cat',  #
+    'aten::clone': 'aten_clone',  # p1
+    'aten::constant_pad_nd': 'aten_constant_pad_nd',  #
     'aten::contiguous': 'aten_contiguous',
-    'aten::copy_': 'aten_copy_',
-    'aten::cumsum': 'aten_cumsum',
-    'aten::div': 'aten_div',
+    'aten::copy_': 'aten_copy_',  # p1
+    'aten::cumsum': 'aten_cumsum',  #
+    'aten::div': 'aten_div',  # p1
     'aten::dropout': 'aten_dropout',
-    'aten::einsum': 'aten_einsum',
-    'aten::embedding': 'aten_embedding',
-    'aten::eq': 'aten_eq',
+    'aten::einsum': 'aten_einsum',  # p1
+    'aten::embedding': 'aten_embedding',  # p1
+    'aten::eq': 'aten_eq',  #
     'aten::expand_as': 'aten_expand_as',
-    'aten::fill_': 'aten_fill_',
-    'aten::floor_divide': 'aten_floor_divide',
-    'aten::full_like': 'aten_full_like',
-    'aten::gather': 'aten_gather',
-    'aten::gelu': 'aten_gelu',
-    'aten::index': 'aten_index',
-    'aten::layer_norm': 'aten_layer_norm',
-    'aten::le': 'aten_le',
-    'aten::log': 'aten_log',
-    'aten::lt': 'aten_lt',
+    'aten::fill_': 'aten_fill_',  #
+    'aten::floor_divide': 'aten_floor_divide',  #
+    'aten::full_like': 'aten_full_like',  #
+    'aten::gather': 'aten_gather',  #
+    'aten::gelu': 'aten_gelu',  # p1
+    'aten::index': 'aten_index',  #
+    'aten::layer_norm': 'aten_layer_norm',  # p1
+    'aten::le': 'aten_le',  #
+    'aten::log': 'aten_log',  #
+    'aten::lt': 'aten_lt',  #
     'aten::masked_fill': 'aten_masked_fill',
     'aten::masked_fill_': 'aten_masked_fill_',
-    'aten::matmul': 'aten_matmul',
-    'aten::max': 'aten_max',
-    'aten::mean': 'aten_mean',
-    'aten::min': 'aten_min',
-    'aten::mul': 'aten_mul',
-    'aten::mul_': 'aten_mul_',
-    'aten::ne': 'aten_ne',
-    'aten::neg': 'aten_neg',
-    'aten::ones': 'aten_ones',
-    'aten::permute': 'aten_permute',
-    'aten::pow': 'aten_pow',
-    'aten::relu': 'aten_relu',
-    'aten::repeat': 'aten_repeat',
-    'aten::reshape': 'aten_reshape',
-    'aten::rsub': 'aten_rsub',
-    'aten::select': 'aten_select',
+    'aten::matmul': 'aten_matmul',  # p1
+    'aten::max': 'aten_max',  #
+    'aten::mean': 'aten_mean',  #
+    'aten::min': 'aten_min',  #
+    'aten::mul': 'aten_mul',  # p1
+    'aten::mul_': 'aten_mul_',  # p1
+    'aten::ne': 'aten_ne',  #
+    'aten::neg': 'aten_neg',  #
+    'aten::ones': 'aten_ones',  #
+    'aten::permute': 'aten_permute',  #
+    'aten::pow': 'aten_pow',  # p1
+    'aten::relu': 'aten_relu',  # p1
+    'aten::repeat': 'aten_repeat',  #
+    'aten::reshape': 'aten_reshape',  #
+    'aten::rsub': 'aten_rsub',  # p1
+    'aten::select': 'aten_select',  #
     'aten::size': 'aten_size',
-    'aten::slice': 'aten_slice',
-    'aten::softmax': 'aten_softmax',
-    'aten::split': 'aten_split',
-    'aten::sqrt': 'aten_sqrt',
-    'aten::squeeze': 'aten_squeeze',
-    'aten::stack': 'aten_stack',
-    'aten::sub': 'aten_sub',
-    'aten::sum': 'aten_sum',
-    'aten::t': 'aten_t',
-    'aten::tanh': 'aten_tanh',
+    'aten::slice': 'aten_slice',  #
+    'aten::softmax': 'aten_softmax',  # p1
+    'aten::split': 'aten_split',  #
+    'aten::sqrt': 'aten_sqrt',  # p1
+    'aten::squeeze': 'aten_squeeze',  #
+    'aten::stack': 'aten_stack',  #
+    'aten::sub': 'aten_sub',  # p1
+    'aten::sum': 'aten_sum',  # p1
+    'aten::t': 'aten_t',  #
+    'aten::tanh': 'aten_tanh',  # p1
     'aten::to': 'aten_to',
-    'aten::transpose': 'aten_transpose',
-    'aten::triu': 'aten_triu',
+    'aten::transpose': 'aten_transpose',  #
+    'aten::triu': 'aten_triu',  #
     'aten::type_as': 'aten_type_as',
-    'aten::unsqueeze': 'aten_unsqueeze',
+    'aten::unsqueeze': 'aten_unsqueeze',  #
     'aten::view': 'aten_view',
-    'aten::where': 'aten_where',
-    'aten::zeros': 'aten_zeros',
-    'aten::zeros_like': 'aten_zeros_like',
+    'aten::where': 'aten_where',  #
+    'aten::zeros': 'aten_zeros',  #
+    'aten::zeros_like': 'aten_zeros_like',  #
     'prim::Constant': 'prim_Constant',
     'prim::GetAttr': 'prim_GetAttr',
     'prim::ListConstruct': 'prim_ListConstruct',
