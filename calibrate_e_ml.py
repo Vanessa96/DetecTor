51366790d140a1b9d486005513ab6ef78935b0f0
ykl7 <yklal95@gmail.com>
2021-01-11 10:31:51 -0500

    e_ml info extracted and script to run it

---
+++ calibrate_e_ml.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = "Yash Kumar Lal"
+
+import os
+import argparse
+import csv
+import time
+import torch
+from transformers import AutoModel
+from transformers import AutoConfig
+
+start_times = dict()
+end_times = dict()
+module_inputs = dict()
+module_outputs = dict()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model-name', type=str, help='Huggingface model name to work with (e.g. bert)', default='bert-base-uncased')
+    parser.add_argument('--batch-size', type=int, help='Batch size of input to run model with', default=1)
+    parser.add_argument('--input-len', type=int, help='Length of input to run model with', default=100)
+    parser.add_argument('--no-cuda', dest='no_cuda', action='store_true', help='Remove use of CUDA')
+    args, _ = parser.parse_known_args()
+    return args
+
+def log_start_builder(name):
+    def log_start(module, m_in):
+        start_times[f'{name}:{module.__class__.__name__}'] = time.perf_counter()
+    return log_start
+
+def log_end_builder(name):
+    def log_end(module, m_in, m_out):
+        end_times[f'{name}:{module.__class__.__name__}'] = time.perf_counter()
+        module_inputs[f'{name}:{module.__class__.__name__}'] = m_in[0].shape
+        module_outputs[f'{name}:{module.__class__.__name__}'] = m_out.shape
+    return log_end
+
+def is_ml_operation(module):
+
+    """
+    This function checks if any given module is of a type that we want to analyse for E_ML operations
+    """
+
+    if isinstance(module, torch.nn.Linear):
+        return True
+    elif isinstance(module, torch.nn.LayerNorm):
+        return True
+    elif isinstance(module, torch.nn.Embedding):
+        return True
+    return False
+
+def calibrate_e_ml(model_name, batch_size, input_len, cuda_available):
+    config = AutoConfig.from_pretrained(model_name)
+    config.torchscript = True
+    model = AutoModel.from_config(config)
+
+    torch.set_grad_enabled(False)
+
+    cuda_exist = torch.cuda.is_available()
+    device = torch.device("cuda" if cuda_exist and not cuda_available else "cpu")
+    model = model.eval().to(device)
+
+    inputs = torch.randint(1000, size=(batch_size, input_len)).long()
+
+    for (name, module) in model.named_modules():
+        if is_ml_operation(module):
+            module.register_forward_pre_hook(log_start_builder(name))
+            module.register_forward_hook(log_end_builder(name))
+
+    loss = model(input_ids=inputs, return_dict=True)
+
+    information = {}
+    for module_name in start_times.keys():
+        module_info = {}
+        module_info['name'] = module_name
+        module_info['inputs'] = module_inputs[module_name]
+        module_info['outputs'] = module_outputs[module_name]
+        module_info['runtime'] = end_times[module_name] - start_times[module_name]
+
+        if 'linear' in module_name.lower():
+            try:
+                information['linear'].append(module_info)
+            except:
+                information['linear'] = [module_info]
+        elif 'layernorm' in module_name.lower():
+            try:
+                information['layernorm'].append(module_info)
+            except:
+                information['layernorm'] = [module_info]
+        elif 'embedding' in module_name.lower():
+            try:
+                information['embedding'].append(module_info)
+            except:
+                information['embedding'] = [module_info]
+
+    return information
+
+def main(args):
+    information = calibrate_e_ml(args.model_name, args.batch_size, args.input_len, args.no_cuda)
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
